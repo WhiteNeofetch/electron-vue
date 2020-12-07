2823ba9b061ac5f282d9ed2a3cfe769d545318d0
js-mord <rumordek@gmail.com>
2020-12-07 05:35:32 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input>
+    <button>ТЫКНУТЬ</button>
   </div>
 </template>
 
@@ -15,6 +17,8 @@ export default {
   }
 }
 </script>
+
+
 
 <style>
 #app {
